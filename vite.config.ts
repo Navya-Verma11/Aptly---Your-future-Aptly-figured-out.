1076b17a6a119e5c7992f075311670d42bd4428c
Navya Verma <118188894+Navya-Verma11@users.noreply.github.com>
2025-11-15 20:46:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Aptly---Your-future-Aptly-figured-out/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
